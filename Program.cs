86f18d7baa8be87f28b25fe34aaf13330311b5e1
Dmitry <profydima@yandex.ru>
2022-12-22 11:15:46 +0300

    решили третью задачу

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача 1
+﻿// Задача 1 Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
 
 System.Console.WriteLine("Введите переменную x: ");
 int x = Convert.ToInt32(Console.ReadLine());
@@ -28,7 +30,8 @@ else if (x > 0 && y < 0)
 }
 
 
-// Задача 2
+// Задача 2 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
 
 System.Console.WriteLine("Введите номер четверти:");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -53,3 +56,24 @@ else
 {
     System.Console.WriteLine("Неверный номер четверти");
 }
+
+// Задача 3 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+
+// ((x1 - x2)^2 + (y1 - y2)^2)^(1/2)
+
+System.Console.WriteLine("Введите координату X точки A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координату X точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2)),2); 
+System.Console.WriteLine($"Расстояние между двумя точками: {result}");
+
+// Задача 4 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
