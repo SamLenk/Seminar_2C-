f0a49f9d07877ac261469e1418ffcf44b3c1bef1
Dmitry <profydima@yandex.ru>
2022-12-22 10:28:18 +0300

    решили первую задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿System.Console.WriteLine("Введите переменную x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите переменную y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x==0 || y==0)
+{
+  System.Console.WriteLine("X и Y не должны равнятся нулю");  
+}
+else if (x > 0 && y > 0)
+{
+   System.Console.WriteLine("Точка находится в первой четверти");   
+}
+else if (x < 0 && y > 0)
+{
+   System.Console.WriteLine("Точка находится в второй четверти");   
+}
+else if (x < 0 && y < 0)
+{
+   System.Console.WriteLine("Точка находится в третьей четверти");   
+}
+else if (x > 0 && y < 0)
+{
+   System.Console.WriteLine("Точка находится в четвертой четверти");   
+}
